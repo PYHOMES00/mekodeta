f59adf84cce4b9e2783fc69dd5d7d5f9e4eef5cd
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-07-12 22:39:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "28243586"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "5340652544"
+SUPPORT_GRP = "the_creator_botz"
+UPDATE_CHNL = "the_creator_botz"
+OWNER_USERNAME = "shubham_X_official"
 
